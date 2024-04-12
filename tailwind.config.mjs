25ed4909cd89c198831dab24fa2fd64e536279db
PabloValles <valles.pablo@hotmail.com>
2024-04-12 17:04:19 -0300

    feat: Se agregan librerías como preline y animaciones, también se crea el navbar y el hero
    WIP

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
+import animations from "@midudev/tailwind-animations";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/preline/preline.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9739C6",
+        secondary: "#F4E56E",
+      },
+    },
+  },
+  plugins: [animations, require("preline/plugin")],
+};
