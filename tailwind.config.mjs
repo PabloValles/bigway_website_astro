6e1068e611a414d8c58919d8448db34a54246e09
PabloValles <valles.pablo@hotmail.com>
2024-04-15 22:08:30 -0300

    Se agregan imagenes y se hacen modificaciones en los componentes. WIP

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,18 @@ export default {
     extend: {
       colors: {
         primary: "#9739C6",
+        "primary-50": "#F5E8FF",
+        "primary-100": "#E1C5FF",
+        "primary-200": "#CD9FFF",
+        "primary-300": "#B97AFF",
+        "primary-400": "#A556FF",
+        "primary-500": "#9739C6",
+        "primary-600": "#7D2F9F",
+        "primary-700": "#622476",
+        "primary-800": "#48194D",
+        "primary-900": "#2D0F26",
+        "primary-950": "#190716",
+
         secondary: "#F4E56E",
       },
     },
